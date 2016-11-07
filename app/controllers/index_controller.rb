a74ea819a1f8b6159150b69f043ed0b884e199a2
rezn5447 <rezn5447@gmail.com>
2016-11-06 19:28:15 -0800

    don't need jquery ui in the layout folder when its already included in the asset pipeline

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,9 +1,9 @@
 class IndexController < ApplicationController
   before_action :define_url
+  
   def index
     @sports = HTTParty.get(@url + "/sports.json")
     @matches = HTTParty.get(@url + "/matches.json")
-
   end
 
   def about
@@ -12,7 +12,7 @@ end
 
 
 
-  private
+private
 
   def define_url
     @url = "https://api-goteam.herokuapp.com/api"
