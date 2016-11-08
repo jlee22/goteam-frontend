eca222e4385f18f40201fa30ffa003fec57b242a
Jeffrey <jfflee.22@gmail.com>
2016-11-08 11:07:30 -0800

    current user works upon login, add skeleton for helpers in sports helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,15 @@
 module UsersHelper
 
   def current_user
-    @current_user
+    user = HTTParty.get("https://api-goteam.herokuapp.com/api/users/#{session[:id]}.json")
+    @current_user||= user if session[:id]
   end
 
   def current_user=(user)
     @current_user = user
   end
 
-  def logged_in?
+  def logged_in
     current_user
   end
 
