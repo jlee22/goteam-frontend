c8cec07ff6ed02713f75a993192cf211ba705a0a
Jeffrey <jfflee.22@gmail.com>
2016-11-08 15:31:56 -0800

    create match helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = UsersHelper.get(current_user['id'])
     @stats = StatsHelper.list(current_user['id'])
-    # @sports = HTTParty.get(@url + "/sports.json")
-    # @sport_names = find_sport(@sports, @stats)
+    @matches = MatchesHelper.list(current_user['id'])
+    p '---------------------------'
+    p @matches
+    p '---------------------------'
   end
 end
--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,8 +2,8 @@ module MatchesHelper
 
   URL = 'https://api-goteam.herokuapp.com/api'
 
-  def self.list
-    @matches = HTTParty.get(URL + "/matches.json")
+  def self.list(current_user_id)
+    @matches = HTTParty.get(URL + "/users/#{current_user_id}/matches.json")
   end
 
   def self.get(id)
