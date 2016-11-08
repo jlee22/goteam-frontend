1818d83c0711334d1242b5751bee35bf498b4c96
Resdan Logan <email": null>
2016-11-08 11:56:47 -0800

    update teams to matches in controler route

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,7 @@ class StatsController < ApplicationController
 
   def update
     p "HIT UPDATE ROUTE!"
-    p HTTParty.put(api_url + "/users/#{params["user_id"]}/stats/#{params["id"]}", body: {active: params["active"]} )
+    p HTTParty.put(api_url + "/users/#{params["user_id"]}/matches/#{params["id"]}", body: {active: params["active"]} )
   end
 
   private
