3efc1b673c82002263802192fa8ebd7e7dcfb6e8
Jeffrey <jfflee.22@gmail.com>
2016-11-08 15:17:37 -0800

    delete extra private method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-    p "****" * 15
-    p current_user['id']
-    p "****" * 15
     @user = UsersHelper.get(current_user['id'])
     @stats = StatsHelper.list(current_user['id'])
     # @sports = HTTParty.get(@url + "/sports.json")
     # @sport_names = find_sport(@sports, @stats)
   end
-end
-
-private
-
-def find_sport(all_sports, user_stats)
-  user_sports = []
-  all_sports.each do |sport|
-    user_stats.each do |stat|
-      if sport['id'] == stat['sport_id']
-        user_sports << sport
-      end
-    end
-  end
-  return user_sports
 end
