9fc948ff26e0fd8552ca9bffa71ae5eaa3c13c16
Jeffrey <jfflee.22@gmail.com>
2016-11-07 17:33:22 -0800

    remove matches in area from login, add register link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
 
   def create
-    p params
-     p sign_in
+    @user = sign_in
+    p @user
+    redirect "/users/#{@user}"
   end
 
   private
 
   def sign_in
-   HTTParty.post('https://api-goteam.herokuapp.com/api/sessions', :body=>{"user"=>{"email"=>'rachael.stammy@vonrueden.info', "password"=>'password'}})
+    HTTParty.post('https://api-goteam.herokuapp.com/api/sessions', :body=>{"user"=>{"email"=>'rachael.stammy@vonrueden.info', "password"=>'password'}})
   end
 
 end
-# params['email']
