723886878e249161bcffa2465d36c085b205e674
Resdan Logan <email": null>
2016-11-07 17:42:37 -0800

    fix merge conflict in index and session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,10 @@
+
 class SessionsController < ApplicationController
 
   def create
-<<<<<<< HEAD
-     p params
-     p sign_in
-=======
     @user = sign_in
     p @user
     redirect "/users/#{@user}"
->>>>>>> aeea90887fe432305d53e57fdc36517db787ce7b
   end
 
   private
