e9750011991431c4fa6c6ce727e98f787c373761
reeses-pieces <nicholasreis@icloud.com>
2016-11-08 17:03:53 -0800

    add css for match show

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
 
   def create
     p 'HIT CREATE ROUTE'
-    @match = HTTParty.post(URL + "/users/#{current_user["id"]}/matchs.json", body: { "sport_id": params["sport_id"] })
+    @match = HTTParty.post(URL + "/users/#{current_user["id"]}/teams.json", body: { "sport_id": params["sport_id"] })
     if @match["message"]
       render json: @match["message"]
     else
