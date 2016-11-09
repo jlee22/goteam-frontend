c9573a6e21e5253974357fa7885c792ec0c80821
Jeffrey <jfflee.22@gmail.com>
2016-11-08 16:20:19 -0800

    add logic on user show to display users upcoming and past matches, delete api_url helper since URL does the same thing - replace api_url to URL in controller except index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   private
 
   def sign_in
-    HTTParty.post(api_url + '/sessions.json', :body=>{"user"=>{"email"=>params['email'], "password"=>params['password']}})
+    HTTParty.post(URL + '/sessions.json', :body=>{"user"=>{"email"=>params['email'], "password"=>params['password']}})
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def api_url
-    'https://api-goteam.herokuapp.com/api'
-  end
-
   def local_url
     'http://localhost:3001/api'
   end
