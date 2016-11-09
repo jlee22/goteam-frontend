6dfa64afcf7ed20adc9706c787b53d3257b9a118
reeses-pieces <nicholasreis@icloud.com>
2016-11-08 16:28:54 -0800

    fix sports js syntax

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,9 +2,9 @@ class TeamsController < ApplicationController
 
   def create
     p 'HIT CREATE ROUTE'
-    @team = HTTParty.post(URL + "/users/#{current_user["id"]}/teams.json", body: { "sport_id": params["sport_id"] })
-    if @team["message"]
-      render json: @team["message"]
+    @match = HTTParty.post(URL + "/users/#{current_user["id"]}/matchs.json", body: { "sport_id": params["sport_id"] })
+    if @match["message"]
+      render json: @match["message"]
     else
       render "/matches/show", layout: false
     end
