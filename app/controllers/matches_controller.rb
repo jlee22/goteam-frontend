82265181f0fa43f70242e18777f7959d9891dc30
Nick Reis <email": null>
2016-11-10 14:58:18 -0800

    add day and time for match show

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -19,6 +19,10 @@ class MatchesController < ApplicationController
   def show
     # @location = HTTParty.get(URL + 'matches/')
     @match = MatchesHelper.get(session, params['id'])
+    date = DateTime.strptime(@match["match"]["date"],"%Y-%m-%d %H:%M")
+    @day = date.strftime("%b %d, %Y")
+    @time = date.strftime("%A %l:%M %p")
+    @location = @match["match"]["location"]
     @sport = SportsHelper.get(@match["match"]["sport_id"])
     @home_team = @match['Home_team']
     @away_team = @match['Away_team']
