4be4eb60329526501095043bdc668d7e32b01e1e
Jeffrey <jfflee.22@gmail.com>
2016-11-09 18:15:17 -0800

    delete extra lines in stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,13 +1,7 @@
 class StatsController < ApplicationController
 
   def update
-    p "HIT UPDATE ROUTE!"
-    # STAT UPDATE NOT WORKING YET
     response = HTTParty.put(URL + "/users/#{params["user_id"]}/stats/#{params["id"]}.json", body:{"active" => params["active"]})
-    p '-----------------------'
-    p response
-    p response['active']
-    p '-----------------------'
   end
 
 end
