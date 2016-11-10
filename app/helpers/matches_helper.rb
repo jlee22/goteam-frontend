394cd53d25902a2a946b67f9cd986a5c3c449456
Jeffrey <jfflee.22@gmail.com>
2016-11-09 22:37:40 -0800

    able to add scores to matches, but still need to get back the right response

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -7,11 +7,17 @@ module MatchesHelper
     HTTParty.get(URL + "/users/"+ session[:id].to_s + "/matches.json?token=" + session[:token].to_s).parsed_response
   end
 
-  def self.get(current_user_id, id)
-    HTTParty.get(URL + "/users/#{current_user_id}/matches/#{id}.json")
+  def self.get(session, id)
+    HTTParty.get(URL + "/users/" + session[:id].to_s + "/matches/#{id}.json?token=" + session[:token].to_s).parsed_response
   end
 
   def self.create(session,sport_id)
     HTTParty.post(URL + "/users/"+ session[:id].to_s + "/teams.json?token=" + session[:token].to_s,body: {sport_id: sport_id}).parsed_response
   end
+
+  def self.put(id, session,home_team,away_team)
+    HTTParty.put(URL + "/users/"+ session[:id].to_s + "/matches/#{id}.json?token=" + session[:token].to_s,body: {"home_team": home_team,
+              "away_team": away_team}).parsed_response
+  end
+
 end
