2dd079a751255fd7a84ad11abbf08f5ff3c186db
Jeffrey <jfflee.22@gmail.com>
2016-11-09 20:29:38 -0800

    fix stats controller find match

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,8 +7,8 @@ class StatsController < ApplicationController
     if params["active"] == "true"
       created_match = MatchesHelper.create(session, @stat["sport_id"])
       if created_match["message"]
-        @message = created_match["message"]
-        @sport = params["sport"]
+        @message = created_match["message"].to_s
+        # @sport = params["sport"]
         render "sports/show"
       else
         @home_team = created_match['Home_team']
