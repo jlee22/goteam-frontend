92cba0c8d8d20a5e56250b300d94991a5bb379bb
Jeffrey <jfflee.22@gmail.com>
2016-11-10 22:05:17 -0800

    add background images to sport index icons and change to 4 column layout in css

--- app/helpers/sports_helper.rb
+++ app/helpers/sports_helper.rb
@@ -22,6 +22,22 @@ module SportsHelper
       "sport_icons/badminton.svg"
     elsif category.include?("table tennis")
       "sport_icons/ping-pong.svg"
+    elsif category.include?("basketball")
+      "sport_icons/basketball.svg"
+    elsif category.include?("bowling")
+      "sport_icons/bowling.svg"
+    elsif category.include?("volley ball")
+      "sport_icons/volleyball.svg"
+    elsif category.include?("golf")
+      "sport_icons/golf.svg"
+    elsif category.include?("rails")
+      "sport_icons/browser.svg"
+    elsif category.include?("flag football")
+      "sport_icons/flag_football.svg"
+    elsif category.include?("ultimate frisbee")
+      "sport_icons/frisbee.svg"
+    elsif category.include?("billiards")
+      "sport_icons/billiards.svg"
     end
   end
 
