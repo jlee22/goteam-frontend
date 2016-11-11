969b8bfcc3dc6d190e322df8741d108c7f9c9ea4
Nick Reis <email": null>
2016-11-10 21:57:15 -0800

    add sport ratings with logic

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,59 +1,96 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function(){
-  $(function () {
-    Highcharts.chart('stats-container', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'My Sport Ratings'
-        },
-        xAxis: {
-            categories: ['Tennis Singles', 'Tennis Doubles', 'Soccer 11 vs 11', 'Soccer 6 vs 6'],
-            title: {
-                text: null
-            }
-        },
-        yAxis: {
-            min: 0,
-            max: 100,
-            title: {
-                text: 'Rating',
-                align: 'high'
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        tooltip: {
-            valueSuffix: ' rating'
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: "Fall 2016",
-            data: [64, 60, 80, 54]
-        }]
-    });
+  // loadBarGraph()
+  getUserStats(user_id, token)
+
 });
-})
+
+var getUserStats = function(user_id, token) {
+  var apiURL = "https://api-goteam.herokuapp.com/api/users/" + user_id + "/stats.json?token=" + token
+  var request = $.ajax({
+    url: apiURL,
+    method: "GET"
+  })
+
+  var ratings = []
+
+  request.done(function(response) {
+    for (var key in response){
+      var obj = {}
+      var name = response[key][1]["category"] +" "+ response[key][1]["name"]
+      var rating = response[key][0]["rating"]
+      obj[name] = rating
+      ratings.push(obj)
+    }
+  loadBarGraph(ratings)
+  })
+
+}
+
+function loadBarGraph(ratings) {
+  var names = []
+  var configRatings = []
+  for(var i = 0; i < ratings.length;i++){
+    names.push(Object.keys(ratings[i])[0])
+    for(var key in ratings[i]){
+      configRatings.push(ratings[i][key])
+    }
+  }
+
+  $(function() {
+    Highcharts.chart('stats-container', {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'My Sport Ratings'
+      },
+      xAxis: {
+        categories: names,
+        title: {
+          text: null
+        }
+      },
+      yAxis: {
+        min: 0,
+        max: 100,
+        title: {
+          text: 'Rating',
+          align: 'high'
+        },
+        labels: {
+          overflow: 'justify'
+        }
+      },
+      tooltip: {
+        valueSuffix: ' rating'
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true
+          }
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: "Fall 2016",
+        data: configRatings
+      }]
+    });
+  });
+};
