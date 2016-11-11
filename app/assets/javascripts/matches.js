4a6eddb62876eee9494d5b6aa9dda2e1a066bede
Nick Reis <email": null>
2016-11-11 11:14:13 -0800

    add slides

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -1,5 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function(){
+  slideHomeTeam()
+  slideAwayTeam()
 
+  var awayTeam = $("#away-team-container")
+  awayTeam.animate( {
+    right: "20%";
+  })
 })
+
+var slideHomeTeam
+var homeTeam = $("#home-team-container")
+  homeTeam.animate( {
+    left: '20%';
+  })
