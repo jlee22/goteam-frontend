90aa566c90f1477d5f5a789d4a7eea508e326c11
Nick Reis <email": null>
2016-11-11 12:03:46 -0800

    add match slide animation

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -3,15 +3,29 @@
 $(document).on('turbolinks:load', function(){
   slideHomeTeam()
   slideAwayTeam()
-
-  var awayTeam = $("#away-team-container")
-  awayTeam.animate( {
-    right: "20%";
-  })
+  fadeInScore()
+  fadeInVs()
 })
 
-var slideHomeTeam
-var homeTeam = $("#home-team-container")
-  homeTeam.animate( {
-    left: '20%';
-  })
+var slideHomeTeam = function() {
+    var homeTeam = $("#home-team-container")
+    homeTeam.animate( {
+      left: '20%'
+    })
+  }
+
+var slideAwayTeam = function() {
+    var awayTeam = $("#away-team-container")
+    awayTeam.animate( {
+      right: '20%'
+    })
+  }
+
+var fadeInScore = function() {
+  $("#match-score-container").children("h4").fadeIn()
+}
+
+var fadeInVs = function() {
+  $("#vs-container").fadeIn("slow")
+}
+
