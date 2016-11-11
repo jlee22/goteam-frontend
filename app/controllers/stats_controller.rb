97f7f2ee579ca9458d4251e38d829d6c71a45fc6
Jeffrey <jfflee.22@gmail.com>
2016-11-11 10:43:04 -0800

    update google api key

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,6 +14,10 @@ class StatsController < ApplicationController
         render "sports/show"
       else
         @match = created_match
+        date = DateTime.strptime(@match["match"]["date"],"%Y-%m-%d %H:%M")
+        @day = date.strftime("%b %d, %Y")
+        @time = date.strftime("%A %l:%M %p")
+        @location = @match["match"]["location"]
         @sport = params["sport"]
         @home_team = created_match['Home_team']
         @away_team = created_match['Away_team']
